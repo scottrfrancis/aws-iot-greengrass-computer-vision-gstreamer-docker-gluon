55173d24b6823a3673b885c135b81a58f919f739
Scott Francis <scott.russell.francis@gmail.com>
2021-06-08 17:03:14 -0700

    make paths username independent

--- go.sh
+++ go.sh
@@ -3,7 +3,7 @@
 cp -ruv artifacts/count_people/* ~/GreengrassCore/artifacts/com.example.count_people/1.0.0/
 cp -ruv recipes/* ~/GreengrassCore/recipes/
 
-sudo /greengrass/v2/bin/greengrass-cli deployment create --recipeDir /home/scott/GreengrassCore/recipes --artifactDir /home/scott/GreengrassCore/artifacts --merge "com.example.count_people=1.0.0"
+sudo /greengrass/v2/bin/greengrass-cli deployment create --recipeDir ~/GreengrassCore/recipes --artifactDir ~/GreengrassCore/artifacts --merge "com.example.count_people=1.0.0"
 
 sleep 30
 sudo /greengrass/v2/bin/greengrass-cli component restart --names=com.example.count_people
